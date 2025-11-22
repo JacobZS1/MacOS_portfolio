8d0806d85691ce7b23da7c92a3a10195d68655d4
DarkOps <jak.zow@wp.pl>
2025-11-22 22:58:29 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { fileURLToPath } from 'url';
+import path from "path";
+
+// naprawa __dirname w ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "#components": resolve(__dirname, "src/components"),
+            "#constants": resolve(__dirname, "src/constants"),
+            "#hoc": resolve(__dirname, "src/hoc"),
+            "#store": resolve(__dirname, "src/store"),
+            "#windows": resolve(__dirname, "windows"),
+        },
+    },
+});
