164daad6c6d1ca80ed58495f5a892c88d6c87c61
JacobZS1 <kuba.zs1.2007@gmail.com>
2025-11-23 16:18:42 +0100

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Navbar from "#components/Navbar.jsx";
+import { Navbar, Welcome, Dock } from "#components";
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <Welcome />
+            <Dock />
         </main>
     );
 };
